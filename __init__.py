a6a777aba964a1f84b01f719ec0243c76d14bf56
marclouzan <87234749+marclouzan@users.noreply.github.com>
2022-10-11 15:02:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,48 +2,126 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
-import scrapy
 from selenium import webdriver
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from scrapy.crawler import CrawlerProcess
 import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import csv
+
+options = webdriver.ChromeOptions()
+#options.add_argument('--headless')
+#options.add_argument('--no-sandbox')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# options.add_argument("user-data-dir=selenium")
+options.add_argument("--remote-debugging-port=9222")
+options.add_argument('--disable-dev-shm-usage')
+
+url = "https://www.fotocasa.es/en" 
+driver = webdriver.Chrome(executable_path = 'chromedriver.exe', options = options)  
+
+driver.implicitly_wait(30)
+driver.get(url) 
+
+# driver.maximize_window()
+driver.find_element_by_xpath('//*[@id="App"]/div[3]/div/div/div/footer/div/button[2]').click()  
+
+#inputing barcelona in the input given on the webpage
+try:
+    input_field = driver.find_element_by_class_name('sui-AtomInput-input sui-AtomInput-input-size-m')   
+except:    
+    input_field = driver.find_element_by_xpath('//*[@id="App"]/div[1]/main/section/div[2]/div/div/div/div/div[2]/div[2]/form/div/div/div/div/div/input')
+# first lets clear the text area incase if anything present in area 
+# inp.clear()  
+# folling command will type barcelona in text area selected.
+input_field.send_keys('Gavà')
+
+driver.find_element_by_xpath('//*[@id="App"]/div[1]/main/section/div[2]/div/div/div/div/div[2]/div[2]/form/button').click()
+
+print(f"reached page {url}")   
 
 
+# for i in range(0,13):  
+#     ActionChains(driver).send_keys(Keys.DOWN).perform()
 
-
-
-
-class fotocasaSpider(scrapy.Spider):
-    
-    name = 'fotocasa'
-    start_urls = [
-        "https://www.fotocasa.es/es/alquiler/viviendas/barcelona-capital/todas-las-zonas/l",
-    ]
-   
-    def parse_itemList(self, response):
+with open('fotocasa.csv','w', encoding = 'utf-8') as f:
+    wr = csv.writer(f, dialect = 'excel')
+    wr.writerow(['Sr.No.','Type','Location', 'Bhk', 'Area', 'Price', 'Decription', 'Broker Name', 'Contact number', 'Link'])
+    n = 0
+    j = 0
+    while(True):
+        # now extract the data from the page we reached on that website 
+        for i in range(0,20):
+            ActionChains(driver).send_keys(Keys.SPACE).perform()
+            time.sleep(2)
+        for i in range(0,195):  
+            ActionChains(driver).send_keys(Keys.UP).send_keys(Keys.UP).perform()
         
-        items =  response.xpath('.//section[@class="re-SearchResult"]')
-        
-        for item in items:
+        props = driver.find_elements_by_class_name('re-CardPackPremium')  
             
-            title = item.css('a::attr(title)').extract()
-            price = item.css('span.re-CardPrice::text').extract()
-            desc = item.css('span.re-CardFeaturesWithIcons-feature-icon.re-CardFeaturesWithIcons-feature-icon--surface::text').extract()
-            phone = item.css('span.sui-AtomButton-text::text').extract()
-            yield {'Text': title,
-                   'Price': price,
-                   'Description': desc,
-                   'Phone': phone}
+        # print(props)
+        for prop in props:  
+            n+=1
+            j+=1
+            # ActionChains(driver).send_keys(Keys.UP).perform()
+            # //*[@id="App"]/div[2]/div[1]/main/div/div[3]/ul/li[7]/a
+            # //*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[10]
+            # time.sleep(3)
+            num = j
+            # //*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[7]/a
             
-        next_page_url = response.xpath('a.sui-AtomButton.sui-AtomButton--primary.sui-AtomButton--outline.sui-AtomButton--center.sui-AtomButton--small.sui-AtomButton--link.sui-AtomButton--empty.sui-AtomButton--rounded::attr(href)').extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(next_page_url, callback=self.parse_itemList)
+            print(prop.find_element_by_xpath(f'//*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[{n}]/div[1]/span/span[1]').text)    
+            try:    
+                name = prop.find_element_by_xpath(f'//*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[{n}]/div[1]/span/span[1]').text
+            except:  
+                name = "" 
+            try:        
+                typeloc = prop.find_element_by_xpath(f'//*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[{n}]/div[2]/a/h3/span[1]').text.split('in ')
+                type = typeloc[0] 
+                loc = typeloc[1] 
+            except:  
+                type = ""  
+                loc = ""    
+            try: 
+                bdrm = int(prop.find_element_by_xpath(f'//*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[{n}]/div/a/ul/li[1]').text.split(' ')[0])
+            except:  
+                bdrm = ""
+            try: 
+                size = prop.find_element_by_xpath(f'//*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[{n}]/div/a/ul/li[3]').text.split(' ')[0]
+            except:   
+                size = ""
+            try:     
+                price = prop.find_element_by_xpath(f'//*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[{n}]/div[2]/a/h3/span[2]/span[1]/span').text    #xpath('re-CardPrice')
+            except:  
+                price = ""    
+            try:  
+
+                desc = prop.find_element_by_xpath(f'//*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[{n}]/div[2]/a/p/span').text
+            except:  
+                desc = ""
+            try:
+                contact = prop.find_element_by_xpath(f'//*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[{n}]/div[2]/div/div[1]/a/span/span[2]').text
+            except:  
+                contact = ""    
+            try:    
+                link = prop.find_element_by_xpath(f'//*[@id="App"]/div[2]/div[1]/main/div/div[2]/section/article[{n}]/a').get_attribute('href')
+            except:  
+                link = ""
+            # print(num, name, loc, price, contact) 
+            wr.writerow([num, type, loc,bdrm,size, price,desc, name, contact, link])  
+
             
-process = CrawlerProcess ({
-    "FEEDS": {"items.json": {"format": "csv", "overwrite": False}},
-    'ROBOTSTXT_OBEY':'False',
-    'USER_AGENT': 'Edge/5.0',
-    
-    })
-process.crawl(fotocasaSpider)
-process.start()
+            # for i in range(0,30):  
+            #     ActionChains(driver).send_keys(Keys.DOWN).perform()
+            # time.sleep(3)
+               
+        # try:  #//*[@id="App"]/div[2]/div[1]/main/div/div[3]/ul/li[7]/a
+        nxtpg_lnk= driver.find_element_by_xpath('//*[@id="App"]/div[2]/div[1]/main/div/div[3]/ul/li[7]/a').get_attribute('href')
+        driver.get(nxtpg_lnk)
+         
+        n = 0
+        # except:  
+            
+        #     print('wasnt able to get to next page!!!!') 
+        #     break   
+# driver.close() # to end all the processes 
+    # ActionChains(driver).send_keys(Keys.UP).perform()
